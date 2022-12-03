d908c94f719ceb423db447df02fd7e3cc855a15d
itai <itaimorag2000@gmail.com>
2022-12-03 19:31:29 +0200

    cahnges in description

--- src/cmps/card-edit.vue
+++ src/cmps/card-edit.vue
@@ -1,74 +1,89 @@
 <template>
-            <div  class="modal-screen" :class="isOn" @click="$emit('toggleEdit')"></div>
-            <article class="modal" :class="isOn">
-                <header class="modal-header edit-block">
-                    <span class="icon lg card"></span>
-                    <input class="header" type="text" v-model="demoCard.title">
-                    <p class="content">in list <span class="move-card-link">traco</span></p>
-                </header>
-                <div class="modal-content flex">
-                    <section class="edit-blocks">
-                        <section class="edit-block">
-                            <span class="icon lg description"></span>
-                            <h3 class="header">Description</h3>
-                            <div class="content fake-text-area fake-button" @click="toggleTextArea">Add a more detailed description…</div>
-                            <!-- <textarea name="" id="" cols="30" rows="3" placeholder="Add a more detailed description…"></textarea> -->
-                        </section>
-                        <section class="edit-block">
-                            <span class="icon lg activity"></span>
-                            <span class="header flex justify-between">
-                                <h3>Activity</h3>
-                                <button class="fake-button">Hide Details</button>
-                            </span>
-                            <ul class="content">
-                                <li class="edit-block">
-                                    <span class="icon lg user"></span>
-                                    <div class="comment-box">
-                                        <textarea type="text" placeholder="Write a comment..."></textarea>
-                                        <footer class="comment-options"></footer>
-                                    </div>
-                                </li>
-                            </ul>
-                        </section>
-                    </section>
-                    <modal-sidebar @updateCard="updateCard" @updateLabels="updateLabels"/>
-                </div>
-            </article>
+    <div class="modal-screen" :class="isOn" @click="$emit('toggleEdit')"></div>
+    <article class="modal" :class="isOn">
+        <header class="modal-header edit-block">
+            <span class="icon lg card"></span>
+            <input class="header" type="text" v-model="demoCard.title">
+            <p class="content">in list <span class="move-card-link">traco</span></p>
+        </header>
+        <div class="modal-content flex">
+            <section class="edit-blocks">
+                <section class="edit-block">
+                    <span class="icon lg description"></span>
+                    <h3 class="header">Description</h3>
+                    <div v-if="!realTextArea" class="content fake-text-area fake-button" @click="toggleTextArea">Add a
+                        more detailed description…</div>
+                    <textarea v-if="realTextArea" name="" id="" cols="30" rows="3"
+                        placeholder="Add a more detailed description…"></textarea>
+                    <button @click="addDescription" class="save-description-btn">Save</button>
+                    <button @click="closeTextArea" class="fake-button">Cancel</button>
+                </section>
+                <section class="edit-block">
+                    <span class="icon lg activity"></span>
+                    <span class="header flex justify-between">
+                        <h3>Activity</h3>
+                        <button class="fake-button">Hide Details</button>
+                    </span>
+                    <ul class="content">
+                        <li class="edit-block">
+                            <span class="icon lg user"></span>
+                            <div class="comment-box">
+                                <textarea type="text" placeholder="Write a comment..."></textarea>
+                                <footer class="comment-options"></footer>
+                            </div>
+                        </li>
+                    </ul>
+                </section>
+            </section>
+            <modal-sidebar @updateCard="updateCard" @updateLabels="updateLabels" />
+        </div>
+    </article>
 </template>
 
 <script>
 import modalSidebar from './modal-sidebar.vue'
-export default{
-    props:{
-        isScreen : Boolean
+export default {
+    props: {
+        isScreen: Boolean
     },
-    emits: ['toggleEdit','updateCard','updateLabels'],
-    data(){
+    emits: ['toggleEdit', 'updateCard', 'updateLabels'],
+    data() {
         return {
             demoCard: {
                 title: 'Make logo',
                 content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-            }
+            },
+            realTextArea: false
         }
     },
-    methods:{
-        toggleTextArea(){
+    created() {
+        this.realTextArea = false
+    },
+    methods: {
+        toggleTextArea() {
+            this.realTextArea = true
             console.log('toggle between real and fake textarea')
         },
-        updateCard(card){
-            this.$emit('updateCard',card)
+        closeTextArea() {
+            this.realTextArea = false
         },
-        updateLabels(labels){
-            this.$emit('updateLabels',labels)
+        addDescription(){
+//need to add description to card
+        },
+        updateCard(card) {
+            this.$emit('updateCard', card)
+        },
+        updateLabels(labels) {
+            this.$emit('updateLabels', labels)
         },
     },
     computed: {
-    isOn() {
-           return { on: this.isScreen === true }
-       },
-   },
-   components: {
-    modalSidebar
-   },
+        isOn() {
+            return { on: this.isScreen === true }
+        },
+    },
+    components: {
+        modalSidebar
+    },
 }
 </script>
