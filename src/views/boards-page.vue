aa901847b938dd8fb672ccb60ec60bc0719ee5b4
itai <itaimorag2000@gmail.com>
2022-12-04 13:40:05 +0200

    hi shachar

--- src/views/boards-page.vue
+++ src/views/boards-page.vue
@@ -1,26 +1,28 @@
 <template>
     <main class="gallery-container flex full">
-      <article class="gallery flex">
-      <aside class="gallery-sidebar">
-        <nav class="flex column">
-          <button class="gallery-nav-btn">
-              <span class="icon sm board"></span>
-              Boards
-            </button>
-          <button class="gallery-nav-btn">
-              <span class="icon sm template"></span>
-            Templates</button>
-          <button class="gallery-nav-btn">
-              <span class="icon sm pulse"></span>
-            Home</button>
-        </nav>
-        <div class="flex justify-between">
-          Workspaces
-          <!-- <img src="../assets/icons/add.svg" alt=""> -->
-        </div>
-      </aside>
+      <section class="main-container-boards">
+
+        <article class="gallery flex">
+          <aside class="gallery-sidebar">
+            <nav class="flex column">
+              <button class="gallery-nav-btn">
+                <span class="icon sm board"></span>
+                Boards
+              </button>
+              <button class="gallery-nav-btn">
+                <span class="icon sm template"></span>
+                Templates</button>
+                <button class="gallery-nav-btn">
+                  <span class="icon sm pulse"></span>
+                  Home</button>
+                </nav>
+                <div class="workspace-div flex justify-between">
+                  Workspaces
+                  <!-- <img src="../assets/icons/add.svg" alt=""> -->
+                </div>
+              </aside>
       
-      <section class="full">
+              <section class="full">
         <h3 class="gallery-header">
           <span class="icon lg time"></span>
           Recently viewed
@@ -69,21 +71,22 @@
           </div>
         </li>
         <!-- <li class="board-preview">Dummy board</li>
-        <li class="board-preview">Dummy board</li>
-        <li class="board-preview">Dummy board</li>
+          <li class="board-preview">Dummy board</li>
+          <li class="board-preview">Dummy board</li>
         <li class="board-preview">Dummy board</li> -->
-
+        
         <!-- <li class="board-preview" v-for="board in boards">
           Dummy board
         </li> -->
       </ul>
     </section>
-
-  </article>
-    </main>
-  
     
-  </template>
+  </article>
+</section>
+</main>
+
+    
+</template>
   
   <script>
   export default {
