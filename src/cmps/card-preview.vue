8ad1c7a593426bca1409d61fd45aac3a63b02667
Alon Hoffman <a.hoffman.lon@gmail.com>
2022-12-04 13:40:04 +0200

    work on colors labels

--- src/cmps/card-preview.vue
+++ src/cmps/card-preview.vue
@@ -1,5 +1,7 @@
 <template>
     <section v-if="!newCard" class="card-preview">
+      <pre v-if="card.labels"  v-for="label in card.labels"
+      :style="('labelColor',label.id)">{{card.labels}}</pre>
       <img v-if="card.imgURL" :src="getCardURL">
       <h1>{{card.title}}</h1>
     </section>
@@ -19,10 +21,24 @@
     computed: {
       getCardURL(){
         return this.card.imgURL
-      }
-    },
+      },
+      created(){
+         console.log( "hi")
+        // setLabelColors(){
+        //   this.card.labels?.forEach(label=>{
+            
+        //   })
+        const labels= this.$store.getters.getCurrBoard.labels
+        console.log("🚀 ~ file: card-preview.vue:31 ~ //setLabelColors ~ labels", labels)
+      },
     methods: {
-      
+      labelColor(id){
+       console.log("🚀 ~ file: card-preview.vue:29 ~ labelColor ~ id", id)
+       const labels= this.$store.getters.getCurrBoard.labels
+       const labelColor= labels.findIndex(label=>label.id===id)
+
+       console.log("🚀 ~ file: card-preview.vue:30 ~ color ~ labelColor", labelColor)
+      }
     }
   
     
