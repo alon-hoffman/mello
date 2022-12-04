13326ea3628ef48648c5d7c0531aade2666cda20
Alon Hoffman <a.hoffman.lon@gmail.com>
2022-12-04 16:16:15 +0200

    color of labels renders

--- src/cmps/card-preview.vue
+++ src/cmps/card-preview.vue
@@ -1,9 +1,17 @@
 <template>
     <section v-if="!newCard" class="card-preview">
-      <!-- <pre v-if="card.labels"  v-for="label in card.labels"
-      :style="('labelColor',label.id)">{{card.labels}}</pre> -->
+     <template v-if="newLabels">
+        <pre   v-for="label in newLabels"
+      :style="{color:label.color}">{{label}}</pre>
+     </template> 
       <img v-if="card.imgURL" :src="getCardURL">
       <h1>{{card.title}}</h1>
+      <div class="icons flex">
+        <div class="left-icons">
+          <span v-if="card.description">description</span>
+          <span v-if="card.checkList">checkList</span>
+        </div>
+      </div>
     </section>
     <section v-else class="card-preview" >
       <textarea v-model="newCard.title" placeholder="Enter a title for this card..." ></textarea>
@@ -18,6 +26,11 @@
     card:Object,
     newCard:Object,
         },
+        data(){
+          return{
+            newLabels:null
+          }
+        },
     computed: {
       getCardURL(){
         return this.card.imgURL
@@ -25,11 +38,9 @@
     },
       created(){
         const boardLabels= this.$store.getters.getCurrBoard.labels
-        console.log("🚀 ~ file: card-preview.vue:28 ~ created ~ boardLabels", boardLabels)
-        this.card.labels?.forEach(label=>{
-          const idx= boardLabels.findIndex(boardLabel=> boardLabel=== label)
-          console.log("🚀 ~ file: card-preview.vue:30 ~ created ~ idx", idx)
-          // label.color= boardLabels[idx].color
+         this.newLabels= this.card.labels?.map(label=>{
+          const idx= boardLabels.findIndex(boardLabel=> boardLabel.id=== label)
+          if(idx>-1)  return {color:boardLabels[idx].color}
         })
       },
   
