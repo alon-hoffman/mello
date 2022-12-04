2a98f0ec11c25981d10d42962c98657cb2c6df7a
Cedrus81 <erez.eitan.97@gmail.com>
2022-12-04 20:33:38 +0200

    starting work on board-creator cmp

--- src/cmps/create-board.vue
+++ src/cmps/create-board.vue
@@ -1,10 +1,20 @@
 <template>
+    <article class="board-creator">
+        <header></header>
+        <div class="preview-display"></div>
+        <section class="background-selection"></section>
+        <input type="text" placeholder="title">
 
+    </article>
 </template>
 
 
 <script>
 export default{
-
+    data(){
+        return{
+            newBoard: {}
+        }
+    }
 }
 </script>
