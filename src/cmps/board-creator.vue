a37a942ea4480d6c1a3c2b17b6781a455c16b682
Cedrus81 <erez.eitan.97@gmail.com>
2022-12-04 23:44:04 +0200

    board creator 2: electric boogaloo

--- src/cmps/board-creator.vue
+++ src/cmps/board-creator.vue
@@ -4,8 +4,10 @@
                 <span class="icon sm close clickable"></span>
 
         </header>
-        <div class="preview-display"></div>
-        <span class="label">backgrounds</span>
+        <div class="preview-display flex align-center justify-center" :style="{'background-color' : newBoard.coverColor}">
+            <img src="https://a.trellocdn.com/prgb/dist/images/board-preview-skeleton.14cda5dc635d1f13bc48.svg" alt="board example">
+        </div>
+        <span class="label">Background</span>
         <section class="background-selection">
             <ul class="custom-images-list">
                 <li>img</li>
@@ -14,18 +16,18 @@
                 <li>img</li>
             </ul>
             <ul class="color-list flex justify-between">
-                <li class="blue-btn clickable"></li>
-                <li class="yellow-btn clickable"></li>
-                <li class="green-btn clickable"></li>
-                <li class="red-btn clickable"></li>
-                <li class="purple-btn clickable"></li>
+                <li class="blue-btn clickable"  @click="setCoverColor('#0079BF')"></li>
+                <li class="yellow-btn clickable"  @click="setCoverColor('#D29034')"></li>
+                <li class="green-btn clickable"  @click="setCoverColor('#519839')"></li>
+                <li class="red-btn clickable"  @click="setCoverColor('#B04632')"></li>
+                <li class="purple-btn clickable"  @click="setCoverColor('#89609e')"></li>
                 <li class="more-btn clickable"></li>
 
             </ul>
         </section>
-        <span class="label">backgrounds</span>
-        <input type="text" v-model="newBoard.title" placeholder="title" required>
-
+        <span class="label">Board title<span class="mandatory">*</span></span>
+        <input type="text" v-model="newBoard.title" required>
+        <button class="save-btn clickable" @click="saveBoard">Create</button>
     </article>
 </template>
 
@@ -33,10 +35,22 @@
 export default{
     data(){
         return{
-            newBoard: {
-                title: '',
-                coverColor: '#FFFFFF'
+            newBoard: null
         }
+    },
+    created(){
+        this.newBoard = {
+                title: '',
+                coverColor: '#0079bf'
+            }
+    },
+    methods:{
+        setCoverColor(clr){
+            this.newBoard.coverColor = clr
+        },
+        saveBoard(){
+            if (!this.newBoard.title) return
+            this.$emit('saveBoard', this.newBoard)
         }
     }
 }
