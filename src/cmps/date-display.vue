54fad277771334489b8292ec3883ffac43740ceb
Alon Hoffman <a.hoffman.lon@gmail.com>
2022-12-05 15:42:26 +0200

    working on show time

--- src/cmps/date-display.vue
+++ src/cmps/date-display.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="date-display">
-<h1>date-display</h1>
+<span><span class="icon sm time" :class="urgency"></span>{{formattedDate}}</span>
     </section>
     
   </template>
@@ -9,7 +9,7 @@
 
   export default {
     props:{
-        date:object
+        date:Number
     },
     data() {
       return {
@@ -18,13 +18,24 @@
     },
     computed: {
     formattedDate(){
-        const second= 1000
+      const second= 1000
         const minute= 60*second
         const hour= 60*minute
         const day= 24*hour
-        const week= 7*day
-        month=30*day
-    }
+        const month= 30*day
+
+        const dateToFormat= new Date(this.date)
+        console.log("🚀 ~ file: date-display.vue:28 ~ formattedDate ~ dateToFormat", dateToFormat)
+  
+        const timeLeft= this.date- Date.now()
+        if(timeLeft>2*day){ 
+
+          //  {"class":"good","date":`${this.date.toLocaleString()}`}
+          }
+        if(timeLeft>day) return "today"
+        return "over-due"
+
+    },
   }
 }
   </script>
