21daceacb913756bd09fa6af9a41f3ed209b9911
Alon Hoffman <a.hoffman.lon@gmail.com>
2022-12-05 13:53:40 +0200

    fixed ghost images

---
+++ src/cmps/date-display.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="date-display">
+<h1>date-display</h1>
+    </section>
+    
+  </template>
+  
+  <script>
+
+  export default {
+    props:{
+        date:object
+    },
+    data() {
+      return {
+       
+      }
+    },
+    computed: {
+    formattedDate(){
+        const second= 1000
+        const minute= 60*second
+        const hour= 60*minute
+        const day= 24*hour
+        const week= 7*day
+        month=30*day
+    }
+  }
+}
+  </script>
+  
