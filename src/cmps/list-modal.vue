2e5522e3dc2ce1f653f5c77374256f4b37289b51
Alon Hoffman <a.hoffman.lon@gmail.com>
2022-12-06 18:00:48 +0200

    work on title modal

--- src/cmps/list-modal.vue
+++ src/cmps/list-modal.vue
@@ -1,7 +1,8 @@
 <template>
-    <section class="list-modal" :style="cords">
-    <!-- <section class="list-modal" :style="{top:(top), left:(right)}"> -->
-<header class="mini-modal-header" >list actions</header>
+    <section class="list-modal flex column" :style="cords" v-click-outside="closeModal">
+    
+    <header class="flex">  <span class="mini-header">list actions <button class="clickable icon ex" @click="closeMiniModal">
+        <span class=""></span></button></span> </header>
 <buttons @click="deleteList" >Archive List</buttons>
 <buttons @click="duplicateList" >Duplicate List</buttons>
     </section>
