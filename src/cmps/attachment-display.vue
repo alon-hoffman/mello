7bd63d2072deb29a78ae169cea95cc859a443626
Alon Hoffman <a.hoffman.lon@gmail.com>
2022-12-06 16:54:52 +0200

    search now has feedback

--- src/cmps/attachment-display.vue
+++ src/cmps/attachment-display.vue
@@ -1,6 +1,27 @@
 <template>
-    <section class="attachment-display">
-<h1>attachment-display</h1>
+    <section class="attachment-display edit-block">
+      <span class="icon lg attachment"></span>
+      <span class="header flex justify-between">
+        <h3>Attachment</h3>
+    </span>
+    <ul class="dynamic-content flex column">
+      <li class="todo-item-container flex clickable" 
+      v-for="attachment, idx in attachments"> 
+      <img :src="attachment.href"/>
+
+      <a
+      :href=attachment.href
+      target="_blank"
+      rel="noreferrer"
+    >
+    <template v-if="attachment.name">{{attachment.name}}</template>
+    <template v-else>{{attachment.type}} file</template>
+    </a>
+
+      
+
+
+    </li></ul>
     </section>
     
   </template>
@@ -20,7 +41,7 @@
       
     },
     created() {
-      
+       console.log(this.attachments)
     },
     methods: {
       
