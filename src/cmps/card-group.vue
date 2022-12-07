a18c5e667dc67cbfa084adb0d28a4a4c6efd378e
itai <itaimorag2000@gmail.com>
2022-12-07 20:38:05 +0200

    new backend

--- src/cmps/card-group.vue
+++ src/cmps/card-group.vue
@@ -44,7 +44,7 @@
   
 <script>
 import cardPreview from "../cmps/card-preview.vue"
-import { boardService } from '../services/board-service-local'
+import { boardService } from '../services/board-service.js'
 import { Container, Draggable } from "vue3-smooth-dnd";
 export default {
   props: {
