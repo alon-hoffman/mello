15f96fe87cf873efe87be34ff512c0f490220a60
itai <itaimorag2000@gmail.com>
2022-12-08 19:12:29 +0200

    added unsplash search

--- src/services/unsplash-photos.service.js
+++ src/services/unsplash-photos.service.js
@@ -8,9 +8,9 @@ export const unsplashPhotosService = {
 }
 
 const ACCESS_KEY='BRK6j-FXov7NV0kG6nXHoRbbZaeFvhwO-AgPQFU7cp0'
-async function getPhoto() {
+async function getPhoto(value='locations') {
     const api=createApi({accessKey:ACCESS_KEY})
-     const res=await api.search.getPhotos({query:'locations'})
+     const res=await api.search.getPhotos({query:value})
     //  console.log(`res.response.results = `, res.response.results)
     return res.response.results
    
