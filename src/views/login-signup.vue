9c420cd6b74ae19a5e614501d97de499ed9953e6
itai <itaimorag2000@gmail.com>
2022-12-08 13:19:27 +0200

    login page

--- src/views/login-signup.vue
+++ src/views/login-signup.vue
@@ -5,24 +5,28 @@
       <!-- logo-login-page -->
       <img class="logo-login-page" src="https://res.cloudinary.com/mello123/image/upload/v1670406786/yd2qyxfpqsuosu76o82o.png" alt="">
     </router-link>
-    <section v-if="!showLogin" class="login-section">
-      <h2>Login</h2>
-      <button class="change-login-btn" @click="changeShowLogin">Click to change to Login/Signup </button>
+    <section v-if="!showLogin" class="login-signup-section">
+      <h2 class="login-signup-title">Log in to Mello</h2>
       <form class="login-signup-form" @submit.prevent="login">
-        <input ref="username" type="text" v-model="credentials.username" placeholder="Username" />
+        <input class="email-input" ref="email" type="text" v-model="credentials.email" placeholder="Enter email" />
         <input type="password" v-model="credentials.password" placeholder="Password" />
         <button class="login-signup-btn">Login</button>
       </form>
+      <div class="change-login-signup-container">
+        <button class="change-login-signup-btn" @click="changeShowLogin">Click to Sign up </button>
+      </div>
     </section>
-    <section v-else class="signup-section">
-      <h2>Signup</h2>
-      <button class="change-login-btn" @click="changeShowLogin">Click to change to Login/Signup </button>
+    <section v-else class="login-signup-section">
+      <h2 class="login-signup-title">Sign up for your account</h2>
       <form class="login-signup-form" @submit.prevent="signup">
         <input type="text" v-model="signupInfo.fullName" placeholder="Full name" />
-        <input type="text" v-model="signupInfo.username" placeholder="Username" />
+        <input class="email-input" type="text" v-model="signupInfo.email" placeholder="Enter email" />
         <input type="password" v-model="signupInfo.password" placeholder="Password" />
         <button class="login-signup-btn">Signup</button>
       </form>
+      <div class="change-login-signup-container">
+        <span class="change-login-signup-btn" @click="changeShowLogin">Click to Log in</span>
+      </div>
     </section>
   </section>
 </template>
@@ -36,12 +40,12 @@ export default {
     return {
       showLogin: false,
       credentials: {
-        username: '',
+        email: '',
         password: ''
       },
       signupInfo: {
         fullName: '',
-        username: '',
+        email: '',
         password: ''
       }
     }
@@ -53,6 +57,9 @@ export default {
     },
     changeShowLogin() {
       this.showLogin = !this.showLogin
+      setTimeout(()=>{
+      this.$refs.email.focus()
+    },0)
     },
     async signup() {
       await this.$store.dispatch({ type: 'signup', cred: this.signupInfo });
@@ -60,7 +67,9 @@ export default {
     },
   },
   mounted() {
-    this.$refs.username.focus()
+    setTimeout(()=>{
+      this.$refs.email.focus()
+    },0)
   }
 
 }
