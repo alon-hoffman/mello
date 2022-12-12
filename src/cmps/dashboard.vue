e0f940e17b8488f5bdfd22772a4ab62f1a7f248e
Cedrus81 <erez.eitan.97@gmail.com>
2022-12-12 09:19:30 +0200

    starting to work on dashboards

---
+++ src/cmps/dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+        <div class="modal-screen" :class="isOn" @click="closeScreen"></div>
+
+</template>
+
+<script>
+export default{
+    isOn() {
+      return {on: this.$store.getters.isScreen}
+    },
+}
+</script>
