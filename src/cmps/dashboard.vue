3bc34238e71f4ace61ebf077c0070ffe82ed9127
Cedrus81 <erez.eitan.97@gmail.com>
2022-12-12 11:49:59 +0200

    work on the daashboard-modal

--- src/cmps/dashboard.vue
+++ src/cmps/dashboard.vue
@@ -1,16 +1,59 @@
 <template>
-        <div class="modal-screen" :class="isOn" @click="closeScreen"></div>
-        <!-- <Bar
-        id="my-chart-id"
-        :options="chartOptions"
-        :data="chartData"
-        /> -->
+        <div class="modal-screen" :class="isOn" @click="toggleScreen">
+            <article class="dashboard-modal">
+                <section class="daily-progress">
+                    <Bar class="demo-chart"
+                         id="my-chart-id"
+                         :options="chartOptions"
+                         :data="chartData"
+                         :style="{'margin': 'auto'}"
+                         />
+                </section>
+                <section class="overall-done"></section>
+                <section class="tasks-per-member"></section>
+                <section class="tasks-per-label"></section>
+                <section class="info"></section>
+               
+            </article>
+        </div>
+
 </template>
 
 <script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
 export default{
-    isOn() {
-      return {on: this.$store.getters.isScreen}
+    components: { Bar },
+    data() {
+        return {
+        chartData: {
+            labels: [ 'January', 'February', 'March' ],
+            datasets: [ { data: [40, 20, 12] } ]
+        },
+        chartOptions: {
+            responsive: true
+        }
+        }
     },
+    methods:{
+        toggleScreen(){
+            this.$store.commit({ type: 'toggleScreen' })
+        },
+    },
+    computed:{
+        isOn() {
+          return {on: this.$store.getters.isScreen}
+        },
+        recentDays(){
+            let time = Date.now()
+            const options = { weekday: 'long', day: 'numeric', month: 'numeric' }
+            const workweek = []
+            for (let i = 0; i < 7; i++){
+                workweek.push(time.toLocaleString(undefined, options))
+            }
+        },
+    }
 }
 </script>
